6a5f001e9f07516f0706a9fdfa2463c405b0659e
Mathieu Drouet <mathieu@drouet.io>
2023-06-07 23:18:22 +0200

    version 1.0 du script

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # audioguides.sh
  
+# Audio Guides Application
+
+This is a Bash script for the Audio Guides Application. It provides a simple command-line interface for selecting and executing a custom command on .wav files in the current directory.
+
+## Features
+
+- Displays a welcome screen to introduce the application.
+- Shows a disclaimer message about the project.
+- Allows selection of a .wav file from the current directory.
+- Executes a custom command on the selected file.
+- Displays contact information for the application.
+- Provides credits for the project.
+
+## Requirements
+
+- Bash shell
+- Whiptail command-line utility
+- `minimodem` command-line utility (if using the provided command)
+
+## Usage
+
+1. Clone the repository or download the script file.
+2. Make the script executable: `chmod +x audioguides.sh`
+3. Run the script: `./audioguides.sh`
+4. Follow the on-screen instructions to navigate through the application.
+5. Select a .wav file to execute the custom command on.
+
+Note: Make sure the necessary requirements are installed and accessible in the system's PATH.
+
+## Customization
+
+You can customize the script by modifying the functions and adding your own commands or functionality. Each function is responsible for a specific task, such as displaying screens or executing commands.
+
+## License
+
+This script is licensed under the [MIT License](LICENSE).
+
+## Author
+
+- [Mathieu](https://github.com/izo)
+
+Feel free to fork, contribute, or provide feedback on the script.
+
--- audioguides.sh
+++ audioguides.sh
@@ -27,92 +27,80 @@ helpline=red,white
 roottext=red,white
 '
 
-display_welcome_screen() {
-	whiptail --title "Welcome" --msgbox "Welcome to the Audio Guides Application. Press 'OK' to continue." 8 60
+# Function to display a welcome screen
+function display_welcome_screen() {
+  whiptail --title "Welcome" --msgbox "Welcome to the Audio Guides Application. Press 'OK' to continue." 8 60
 }
 
-display_disclaimer() {
-	disclaimer_text="Audioguid.es est un projet photographique. Plus d'information sur audioguid.es."
-	whiptail --title "Disclaimer" --msgbox "$disclaimer_text" 10 60
+# Function to display a disclaimer
+function display_disclaimer() {
+  local disclaimer_text="Audioguid.es est un projet photographique. Plus d'information sur audioguid.es."
+  whiptail --title "Disclaimer" --msgbox "$disclaimer_text" 10 60
 }
 
-# The open_local_file function allows the user to select a file from the script's directory
-# using a whiptail menu. Once the user selects a file and confirms, the function compresses
-# the selected file as a ZIP file using the 'zip' command. While the file is being compressed,
-# a progress bar is displayed using a whiptail gauge. After the compression is complete,
-# a message is shown to inform the user about the successful compression.
-
-open_local_file() {
-  files=()
-  while IFS= read -r -d $'\0' file; do
-	files+=("${file##*/}")
-  done < <(find . -maxdepth 1 -type f -print0)
-
-  selected_file=$(whiptail --title "File Selector" --menu "Choose a file:" 15 60 5 "${files[@]}" 3>&1 1>&2 2>&3)
-
-  if [ $? -eq 0 ]; then
-	if (whiptail --title "Confirmation" --yesno "You selected '$selected_file'. Press 'Y' to compress it as a ZIP file." 10 60); then
-	  echo "Compressing '$selected_file'..."
-
-	  # Get the file size for the progress bar
-	  file_size=$(stat -c%s "$selected_file")
-
-	  # Compress the selected file as a ZIP file and display the progress bar
-	  (pv -n -s "$file_size" "$selected_file" | zip "${selected_file}.zip" -) 2>&1 | whiptail --gauge "Please wait while the file is being compressed..." 6 50 0
-
-	  # Show a message when the compression is complete
-	  whiptail --title "Result" --msgbox "'$selected_file' has been compressed as '${selected_file}.zip'." 8 40
-	else
-	  echo "User chose not to compress the file."
-	fi
-  else
-	whiptail --title "Result" --msgbox "User canceled the file selection." 8 40
-  fi
-}
-
-
-	
-display_contact_info() {
-	contact_info="Name: Mathieu Drouet \nWebsite: https://www.audioguid.es\nEmail: mathieu@drouet.io"
-	whiptail --title "Contact Information" --scrolltext --msgbox "$contact_info" 10 60
-}
-
-
-display_credits() {
-	whiptail --title "Credits" --scrolltext --msgbox "Credits:\nThis script was created by Mathieu Drouet.\nSpecial thanks to: \n- Stephane Maguet \n- Meanignful \n- Agathe Vuachet \n- Olivier (DaffyDuke) Duquesne \n- Kamal Mostafa \n- Crumplepop \n- Robert Klebe \n- Mike Kohn" 17 60
-}
-
-# Replace while loop with main_menu function
-main_menu() {
+# Function to select a .wav file from the script's directory and execute a custom command
+function open_local_file() {
   while true; do
-	action=$(whiptail --title "Main Menu" --menu "Choose an action:" 16 60 4 \
-	  "1" "Display a disclaimer" \
-	  "2" "Open a local file" \
-	  "3" "Display contact information" \
-	  "4" "Display credits" \
-	  3>&1 1>&2 2>&3)
+    local files=()
+    local i=0
+    while IFS= read -r -d $'\0' file; do
+      files+=("$((++i))" "${file##*/}")
+    done < <(find . -maxdepth 1 -type f -name "*.wav" -print0)
 
-	exit_status=$?
-	if [ $exit_status -eq 0 ]; then
-	  case $action in
-		1)
-		  display_disclaimer
-		  ;;
-		2)
-		  open_local_file
-		  ;;
-		3)
-		  display_contact_info
-		  ;;
-		4)
-		  display_credits
-		  ;;
-	  esac
-	else
-	  exit 0
-	fi
+    # Check if there are any .wav files before attempting to display them in a menu
+    if [ ${#files[@]} -eq 0 ]; then
+      whiptail --title "No Files" --msgbox "There are no .wav files in the current directory." 10 60
+      break
+    fi
+
+    local selected_file_index=$(whiptail --title "File Selector" --menu "Choose a file:" 15 60 5 "${files[@]}" 3>&1 1>&2 2>&3)
+
+    if [ $? -eq 0 ]; then
+      local selected_file=${files[2*$selected_file_index-1]}
+      if (whiptail --title "Confirmation" --yesno "You selected '$selected_file'. Press 'Y' to execute the command." 10 60); then
+        echo "Executing command on '$selected_file'..."
+
+        # Execute the command and check its result
+        if (minimodem -r 1200 -f "$selected_file" --rx-one | tar -C ./ -xzvf -); then
+          # Show a message when the execution is complete
+          whiptail --title "Result" --msgbox "Command executed on '$selected_file'." 8 40
+          break
+        else
+          whiptail --title "Error" --msgbox "Failed to execute command on '$selected_file'." 8 40
+        fi
+      else
+        echo "User chose not to execute the command."
+      fi
+    else
+      if (whiptail --title "File selection" --yesno "No file selected. Would you like to try again?" 8 40); then
+        continue
+      else
+        break
+      fi
+    fi
   done
 }
 
-display_welcome_screen
-main_menu
+# Function to display contact information
+function display_contact_info() {
+  local contact_info="Name: Mathieu Drouet \nWebsite: https://www.audioguid.es\nEmail: mathieu@drouet.io"
+  whiptail --title "Contact Information" --yesno "$contact_info" --no-button "Continue" --yes-button "Quit" 10 60
+  if [ $? -eq 0 ]; then
+    exit 1
+  fi
+}
+
+# Function to display credits
+function display_credits() {
+  local credits="Crédits : \nMathieu Drouet\nhttps://www.audioguid.es"
+  whiptail --title "Credits" --msgbox "$credits" 10 60
+}
+
+# Main script loop. This will continue to run the script until the user chooses to quit
+while true; do
+  display_welcome_screen
+  display_disclaimer
+  open_local_file
+  display_contact_info
+  display_credits
+done
